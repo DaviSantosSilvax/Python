8a3ed6e5818f835e0979c612db67e27ccd6f09ab
DaviSantosSilvax <xkmaluffy@gmail.com>
2025-12-26 18:32:19 -0300

    Organizar estrutura do curso Python (Mundo 1 e Mundo 2)

---
+++ Curso-Em-Video-Python/Mundo1/Metro para cm e mm.py
@@ -0,0 +1,7 @@
+print("Me diga quantos metros e converterei em centimetros e milimetros")
+
+m = float(input("Quantos metros? "))
+cm = (m*100)
+mm =  (cm*10)
+
+print(f"{m} metros são {cm} centimetros e {mm} milimetros")
